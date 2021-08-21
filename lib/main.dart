bfa763464920bff73fc03f0fe66a35e44e3baf0d
aakzsh <aakashferrari@gmail.com>
2021-08-21 14:28:16 +0530

    add: launcher icon, splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:adaptive_theme/adaptive_theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,11 +13,48 @@ class App extends StatefulWidget {
   _AppState createState() => _AppState();
 }
 
+Color mainbg = Color.fromRGBO(242, 170, 76, 1);
+Color maintext = Color.fromRGBO(16, 24, 32, 1);
+
 class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Ease Access",
+    return AdaptiveTheme(
+      light: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.amber,
+        accentColor: Colors.amber,
+      ),
+      dark: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blueGrey,
+        accentColor: Colors.amber,
+      ),
+      initial: AdaptiveThemeMode.light,
+      builder: (theme, darkTheme) => MaterialApp(
+        title: 'Ease Access',
+        theme: theme,
+        darkTheme: darkTheme,
+        home: HomePage(),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Text("Hello"),
+        ),
+      ),
     );
   }
 }
